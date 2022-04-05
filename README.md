3704df9bd5d6ce16c1a61922b105b1893bdb5d8a
Hongzhi Li <hongzhi.li@senior798.eu>
2022-04-05 21:53:23 +0200

    Finished backend api tests and make counter works

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # azure-resume
 
-My own azure resume, following [ACG project video.](youtube.com/acloudguru)
-
+My own azure resume
 ## First steps
 
 - Frontend folder contains the website.
 - main.js contains visitor counter code.
+- Back-end on Azure CosmosDB
--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -17,24 +17,22 @@ namespace Company.Function
         [FunctionName("GetResumeCounter")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id="1", PartitionKey ="1")] Counter counter,
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id="1", PartitionKey ="1")] out Counter updatedCounter,
             ILogger log)
         {
-            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            updatedCounter = counter;
-            updatedCounter.Count += 1;
+           updatedCounter= counter;
+           updatedCounter.Count += 1;
 
-            var jsonToRetun = JsonConvert.SerializeObject(counter);
+           var jsonToRetun = JsonConvert.SerializeObject(counter);
+
 
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
                 Content = new StringContent(jsonToRetun, Encoding.UTF8, "application/json")
             };
-
-            
         }
     }
 }
